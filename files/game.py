51ae914bc781fc6ec40921f68957f3f184ceeb3f
Maros <maros3845@gmail.com>
2022-02-17 21:29:11 +0100

    setting up

--- files/game.py
+++ files/game.py
@@ -4,9 +4,18 @@ from files.canvas_obj import object
 class Game(object):
     def __init__(self) -> None:
         super().__init__()
+        pygame.init()
         self.screen = pygame.display.set_mode((self.WIDTH, self.HEIGHT))
+        self.quit = False
 
-
-    def run():
-        while True:
-            pass
+    def event(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.quit = True
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    self.quit = True
+    def run(self):
+        while not self.quit:
+            self.event()
+            pygame.display.update()
--- run.py
+++ run.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+from os import environ
+environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'
 from files.__main__ import main
 
 main()
