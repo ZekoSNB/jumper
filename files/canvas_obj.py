086e9dc96de830a0dc4e91302a94be67dbb122bd
Maros <maros3845@gmail.com>
2022-02-17 20:50:17 +0100

    first Run

---
+++ files/canvas_obj.py
@@ -0,0 +1,6 @@
+import pygame
+
+
+class object():
+    def __init__(self) -> None:
+        self.WIDTH, self.HEIGHT = 1280,720
---
+++ files/game.py
@@ -0,0 +1,12 @@
+import pygame
+from files.canvas_obj import object
+
+class Game(object):
+    def __init__(self) -> None:
+        super().__init__()
+        self.screen = pygame.display.set_mode((self.WIDTH, self.HEIGHT))
+
+
+    def run():
+        while True:
+            pass
