b713ca9c79d690f0a13e345c24efc46ea32a3268
Maros <maros3845@gmail.com>
2022-02-17 20:40:34 +0100

    Adding Files

---
+++ files/__main__.py
@@ -0,0 +1,10 @@
+from files.game import Game
+
+def main():
+    g = Game()
+    g.run()
+    return 0 
+
+
+if __name__ == '__main__':
+    main()
---
+++ run.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+from files.__main__ import main
+
+main()
